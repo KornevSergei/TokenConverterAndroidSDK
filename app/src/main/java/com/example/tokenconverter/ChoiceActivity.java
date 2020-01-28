b6a828123e72d2f34e2c67b68987971071a0bbad
KornevSergei <4Kornev@gmail.com>
2020-01-28 16:38:19 +0300

    Step 6

--- app/src/main/java/com/example/tokenconverter/ChoiceActivity.java
+++ app/src/main/java/com/example/tokenconverter/ChoiceActivity.java
@@ -12,8 +12,6 @@ public class ChoiceActivity extends AppCompatActivity {
     int quantityToken = 0;
     int quantityOne = 0;
     int quantityTwo = 0;
-    //результат
-    TextView tokenAmount;
 
 
     @Override
@@ -58,18 +56,17 @@ public class ChoiceActivity extends AppCompatActivity {
         if (quantityOne < 0) {
             quantityOne = 0;
         }
-            TextView counter1 = findViewById(R.id.initialToProduct1);
-            counter1.setText("" + quantityOne);
-            orderPrice();
-        }
-
-
+        TextView counter1 = findViewById(R.id.initialToProduct1);
+        counter1.setText("" + quantityOne);
+        orderPrice();
+    }
 
     // создаём действие кнопки Плюс
     public void increaseQuantityTwo(View view) {
         quantityTwo++;
         TextView counter2 = findViewById(R.id.initialToProduct2);
         counter2.setText("" + quantityTwo);
+        orderPrice();
     }
 
     // создаём действие кнопки Минус
@@ -81,12 +78,20 @@ public class ChoiceActivity extends AppCompatActivity {
         }
         TextView counter2 = findViewById(R.id.initialToProduct2);
         counter2.setText("" + quantityTwo);
+        orderPrice();
     }
 
     public void orderPrice() {
 //   сразу меняем значение цены при нажатии
         TextView tokenAmount = findViewById(R.id.tokenAmount);
-        tokenAmount.setText("" + quantityOne);
+        //Приводимс тринг к Инт
+        int a = Integer.parseInt(String.valueOf(quantityOne));
+        int b = Integer.parseInt(String.valueOf(quantityTwo));
+//        tokenAmount.setText("" + quantityOne + "" + quantityTwo);
+        //считаем сумму и приводим к строке
+        tokenAmount.setText("" + (a + (b * 2)));
+
+
     }
 
 }
